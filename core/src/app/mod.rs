96bc9f393c490a1de8fa9587e51b61c1d0b6c9fa
Michael Shaw <michaeljshaw@gmail.com>
2017-09-28 15:20:16 +1000

    Shaking out app traits a bit

---
+++ core/src/app/mod.rs
@@ -0,0 +1,28 @@
+use std::fmt::Debug;
+
+use std::collections::BTreeMap as Map;
+
+use Tick;
+use std::hash::Hash;
+
+use serde::Serialize;
+use serde::de::DeserializeOwned;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub enum Event<Id, Entity, EntityEvent, RenderEvent>  {
+    SpawnEvent(Id, Entity),
+    Delete(Id),
+    DeleteRange(Id, Id),
+    EntityEvent(Id, EntityEvent),
+    RenderEvent(RenderEvent),
+}
+
+pub trait App {
+    type Id : Clone + Hash + Debug + Eq + Ord + Serialize + DeserializeOwned;
+    type Entity : Clone + Debug + Serialize + DeserializeOwned; // do we need Eq?
+    type EntityEvent : Clone + Debug + Eq + Ord + Serialize + DeserializeOwned;
+    type RenderEvent : Clone + Debug + Eq + Ord + Serialize + DeserializeOwned;
+
+    fn handle_entity_event(event:&Self::EntityEvent, entity: &mut Self::Entity) -> Vec<Event<Self::Id, Self::Entity, Self::EntityEvent, Self::RenderEvent>>;
+    fn simulate(time:Tick, state:&Map<Self::Id, Self::Entity>, id: &Self::Id, entity: &Self::Entity) -> (Vec<Self::EntityEvent>, Vec<Event<Self::Id, Self::Entity, Self::EntityEvent, Self::RenderEvent>>);
+}
--- puck/src/app/mod.rs
+++ puck/src/app/mod.rs
@@ -1,67 +1,65 @@
-use puck_core::HashMap;
+use puck_core::{HashMap, Tick};
+use puck_core::app::{Event, App};
 use input::Input;
 
 use std::hash::Hash;
 use std::fmt::Debug;
 
+use std::collections::BTreeMap;
+
 use dimensions::Dimensions;
 
+use render::gfx::{Renderer, construct_opengl_renderer};
+use gfx;
+use FileResources;
+use PuckResult;
+
 // - abstract trait of EventSink?
-// - authority function? view function? (combine the two?)
-//   Id -> Change/View/None (for a given ... player)
 // - how do we manage identifiers? ... across kinds?
 // - how do we determine viewability of render events?
 // - initial state?
 // - notions of client identity?
-// - move state to tree map?
+// - move entity state to tree map ...
 
 // how does a server request/force a change to something it doesn't own? ... how does this affect ordering?
-
 // how does client and server negotiate over player location? In PUBG, how do we put the player in the plane (with location being client side)
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Event<Id, Entity, EntityEvent, RenderEvent>  {
-    SpawnEvent(Id, Entity),
-    Delete(Id),
-    DeleteRange(Id, Id),
-    EntityEvent(Id, EntityEvent),
-    RenderEvent(RenderEvent),
-}
+// "route", who's the owner of this entity
+// "visibility" server notion foreach client
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum Authority {
-    Change,
-    View,
-    None,
-} // how does the server interpret being order for being able to change a remote actor? ... maybe it's more of a routing function ...
-
-pub type Time = u64;
-
-pub struct RenderConfig {
+#[derive(Clone)]
+pub struct RenderSettings {
     pub dimensions: (u32, u32),
+    pub vsync: bool,
     pub title: String,
 }
 
-pub trait App {
-    type Id : Clone + Hash + Debug + Eq + Ord;
-    type Entity : Clone + Debug; // do we need Eq?
-    type EntityEvent : Clone + Debug + Eq + Ord;
-    type RenderEvent : Clone + Debug + Eq + Ord;
-
-
-
-    fn handle_entity_event(event:&Self::EntityEvent, entity: &mut Self::Entity) -> Vec<Event<Self::Id, Self::Entity, Self::EntityEvent, Self::RenderEvent>>;
-    fn simulate(time:Time, state:&HashMap<Self::Id, Self::Entity>, id: &Self::Id, entity: &Self::Entity) -> (Vec<Self::EntityEvent>, Vec<Event<Self::Id, Self::Entity, Self::EntityEvent, Self::RenderEvent>>);
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct SimSettings {
+    pub ticks_per_second: u64,
 }
 
 pub trait RenderedApp : App {
     type RenderState;
 
-    fn render_config() -> RenderConfig;
     fn handle_input(input:&Input, dimensions: &Dimensions, state: &HashMap<Self::Id, Self::Entity>) -> Vec<Event<Self::Id, Self::Entity, Self::EntityEvent, Self::RenderEvent>>;
-//    fn simulate(time:Time, state:&HashMap<Self::Id, Self::Entity>, entity: &Self::Entity) -> Vec<Self::Event>;
-
     fn handle_render_event(event: &Self::RenderEvent, &mut Self::RenderState);
     fn render(state:&HashMap<Self::Id, Self::Entity>, &mut Self::RenderState);
 }
 
+pub struct ReneredAppRunner<RA, R, C, F, D> where RA : RenderedApp,
+                                                  R : gfx::Resources,
+                                                  C : gfx::CommandBuffer<R>,
+                                                  F : gfx::Factory<R>,
+                                                  D : gfx::Device {
+    entities: BTreeMap<RA::Id, RA::Entity>,
+    renderer: Renderer<R, C, F, D>,
+    render_state: RA::RenderState,
+}
+
+pub fn run<RA>(app: RA, file_resources:FileResources, sim_settings: SimSettings, render_settings:RenderSettings, render_state: RA::RenderState) -> PuckResult<()> where RA : RenderedApp {
+    let renderer = construct_opengl_renderer(file_resources, render_settings.dimensions, render_settings.vsync, &render_settings.title)?;
+
+
+    Ok(())
+}
